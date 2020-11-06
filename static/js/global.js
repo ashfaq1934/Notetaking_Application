677a15a95bc48ba9eb750af2fe63418e0f359528
Ashfaq Ahmed <ashfaq@pop-os.localdomain>
2020-11-06 17:59:10 +0000

    implemented client side javascript input sanitization

--- static/js/global.js
+++ static/js/global.js
@@ -1,10 +1,10 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
@@ -12,3 +12,48 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function validateForm() {
+  let form = document.forms[0];
+  let title = form.querySelector('input[name="title"]');
+  let data = form.querySelector('textarea[name="editordata"]');
+  let term = form.querySelector('textarea[name="term"]');
+  let definition = form.querySelector('textarea[name="definition"]');
+
+  if (title !== null){
+    let check = checkForEvil(title.value);
+    if (check === false){
+      alert('Booooooooo');
+      event.preventDefault();
+    }
+  }
+  if (data !== null){
+    let check = checkForEvil(data.value);
+    if (check === false){
+      alert('Booooooooo');
+      event.preventDefault();
+    }
+  }
+  if (term !== null){
+    let check = checkForEvil(term.value);
+    if (check === false){
+      alert('Booooooooo');
+      event.preventDefault();
+    }
+  }
+  if (definition !== null){
+    let check = checkForEvil(definition.value);
+    if (check === false){
+      alert('Booooooooo');
+      event.preventDefault();
+    }
+  }
+}
+
+function checkForEvil(carrier) {
+	if (carrier.indexOf("<script>") > -1) { return false; }
+	if (carrier.indexOf(":javascript") > -1) { return false; }
+	if (carrier.indexOf("javascript:") > -1) { return false; }
+	if (carrier.indexOf("function()") > -1) { return false; }
+}
+
