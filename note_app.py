795add951b2bb25ea50b944d85774fb2bfc35087
Ashfaq Ahmed <ashfaq@pop-os.localdomain>
2020-11-08 16:01:02 +0000

    implemented feature to delete a collection and all of its resources

--- note_app.py
+++ note_app.py
@@ -321,7 +321,7 @@ def create_deck():
         db_session.add(deck)
         db_session.commit()
         flash('Deck Saved!')
-        return redirect(url_for('view_deck', uuid=deck_uuid))
+        return redirect(url_for('create_flashcard'))
     return render_template('create_deck.html')
 
 
@@ -529,19 +529,66 @@ def delete_note(uuid):
         .filter(Note.uuid == uuid).first()
     db_session.delete(note)
     db_session.commit()
-    flash(f'Note {note.title} deleted')
+    flash(f'{note.title} deleted')
     return redirect(url_for('root'))
 
 
 @app.route('/delete/flashcard/<uuid>/')
 def delete_flashcard(uuid):
-    flashcard = db_session.query(Flashcard).join(Collection, Deck.collection_id == Collection.id) \
-        .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
-        .filter(Collection.user_id == User.id).filter(Deck.collection_id == Collection.id) \
+    flashcard = db_session.query(Flashcard).join(Deck, Deck.id == Flashcard.deck_id)\
+        .join(Collection, Deck.collection_id == Collection.id).join(User, Collection.user_id == User.id)\
+        .filter(User.email == session['user']).filter(Collection.user_id == User.id)\
+        .filter(Deck.collection_id == Collection.id).filter(Flashcard.deck_id == Deck.id)\
         .filter(Flashcard.uuid == uuid).first()
+
+    deck = db_session.query(Deck).filter(Deck.id == flashcard.deck_id).first()
     db_session.delete(flashcard)
     db_session.commit()
-    flash(f'Note {flashcard.title} deleted')
+    flash(f'{flashcard.title} deleted')
+    return redirect(url_for('view_deck', uuid=deck.uuid))
+
+
+@app.route('/delete/deck/<uuid>/')
+def delete_deck(uuid):
+    deck = db_session.query(Deck).join(Collection, Deck.collection_id == Collection.id) \
+        .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
+        .filter(Deck.uuid == uuid).first()
+    try:
+        flashcards = db_session.query(Flashcard).filter(Flashcard.deck_id == deck.id).delete()
+    except:
+        flashcards = None
+    db_session.delete(deck)
+    db_session.commit()
+    flash(f'{deck.title} deleted')
+    return redirect(url_for('root'))
+
+
+@app.route('/delete/collection/<uuid>/')
+def delete_collection(uuid):
+    collection = db_session.query(Collection).join(User, Collection.user_id == User.id) \
+        .filter(User.email == session['user']).filter(Collection.uuid == uuid).first()
+
+    try:
+        notes = db_session.query(Note).filter(Note.collection_id == collection.id).delete()
+        db_session.commit()
+    except:
+        notes = None
+
+    try:
+        deck_items = db_session.query(Deck).filter(Deck.collection_id == collection.id).all()
+        decks = db_session.query(Deck).filter(Deck.collection_id == collection.id).delete()
+
+        for deck in deck_items:
+            flashcards = db_session.query(Flashcard).filter(Flashcard.deck_id == deck.id).delete()
+            db_session.commit()
+        db_session.commit()
+    except:
+        decks = None
+
+    db_session.delete(collection)
+    db_session.commit()
+
+    flash(f'{collection.title} deleted')
     return redirect(url_for('root'))
 
 
