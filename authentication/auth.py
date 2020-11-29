d7c64f72f0a6f74ef7a44246df41016a0149db78
Ashfaq Ahmed <ashfaq@pop-os.localdomain>
2020-11-29 21:20:15 +0000

    added comments to code and fixed bug with decks

--- authentication/auth.py
+++ authentication/auth.py
@@ -9,23 +9,27 @@ import bleach
 from dotenv import load_dotenv
 import os
 
+# Register as blueprint
 authentication = Blueprint('auth', __name__, url_prefix='/account/', template_folder='templates')
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+# Load parent directory of application and load the .env file
 PARENT_DIR = os.path.abspath(os.curdir)
 load_dotenv(os.path.join(PARENT_DIR, '.env'))
+
+# Get data configuration settings and create the database URI
 db_host = os.getenv("DATABASE_HOST")
 db_user = os.getenv("DATABASE_USER")
 db_password = os.getenv("DATABASE_PASSWORD")
 db_name = os.getenv("DATABASE_NAME")
 database_uri = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
 
-
+# Connect to the database
 engine = create_engine(database_uri)
 
+# Bind session maker to engine
 Session = sessionmaker(bind=engine)
 
-
+# Flask view decorator to return the user to the login page if they aren't authenticated
 def requires_login(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -35,22 +39,25 @@ def requires_login(f):
         return f(*args, **kwargs)
     return decorated
 
-
+# Flask decorator for the user's account page
 @authentication.route('/', methods=['GET', 'POST'])
 @requires_login
 def account():
+    # Create database session and get the authenticated user's details
     db_session = Session()
     user = db_session.query(User).filter(User.email == session['user']).first()
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+        # If the email was changed, sanitize the input and modify the User object
         if email:
             user.email = bleach.clean(email)
             db_session.commit()
             session['user'] = bleach.clean(email)
             flash('Email saved')
             return redirect(url_for('auth.account'))
-
+        # If the password was changed, sanitize the input and modify the User object
         if password:
             hashed = bcrypt.hashpw(bleach.clean(password).encode('utf-8'), bcrypt.gensalt())
             user.password = hashed
@@ -61,27 +68,31 @@ def account():
     return render_template('account.html', user=user)
 
 
+# Flask decorator for registration page
 @authentication.route('/register/', methods=['GET', 'POST'])
 def register():
+    # Create database session
     db_session = Session()
     if request.method == 'POST':
         try:
+            # Sanitize inputs
             email = bleach.clean(request.form['email'])
             password = bleach.clean(request.form['password'])
-
+            # if email or password is empty, redirect back to registration page
             if not email:
                 flash('Missing email')
                 return redirect(url_for('auth.register'))
             if not password:
                 flash('Missing password')
                 return redirect(url_for('auth.register'))
-
+            # Query User table for existing users with the same email, and if they exist, redirect back to registration
             user = db_session.query(User).filter(User.email == email).count()
-
             if user >= 1:
                 flash('Account  already exists')
                 return redirect(url_for('auth.register'))
             else:
+                # If the user doesnt exist, create a new one with the email and password input field
+                # Hash and salt the password before commit the user to the database
                 hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
                 user = User(email=email, password=hashed)
                 db_session.add(user)
@@ -95,20 +106,22 @@ def register():
     return render_template('register.html')
 
 
+# Flask decorator login page
 @authentication.route('/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         try:
+            # Sanitize inputs
             email = bleach.clean(request.form['email'])
             password = bleach.clean(request.form['password'])
-
+            # if email or password is empty, redirect back to login page
             if not email:
                 flash('Missing email')
                 return redirect(url_for('auth.login'))
             if not password:
                 flash('Missing password')
                 return redirect(url_for('auth.login'))
-
+            # Use check_auth method to check if the credentials match, if the method returns true, create the session
             if check_auth(email, password):
                 session['logged_in'] = True
                 session['user'] = email
@@ -123,6 +136,8 @@ def login():
     return render_template('login.html')
 
 
+# Flask route for logout
+# Delete session and redirect to root page
 @authentication.route('/logout/')
 def logout():
     session['logged_in'] = False
@@ -131,15 +146,21 @@ def logout():
     return redirect(url_for('root'))
 
 
+# Method for checking if the login form input matches the user's credentials
 def check_auth(email, password):
+    # Create database session
     db_session = Session()
     try:
+        # Query the User table and filter the objects by the inputted email
         user = db_session.query(User).filter(User.email == email).first()
+        # Use bcrypt's check password method to hash the password input and see if they match
         check = bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8'))
 
+        # If the check passes return true, otherwise return false
         if check:
             return True
         return False
     except:
-        flash("User doesn't exist, have you registeared?")
+        # If the user query fails, return false
+        flash("User doesn't exist, have you registered?")
         return False
--- note_app.py
+++ note_app.py
@@ -11,11 +11,15 @@ from delete.delete import delete
 from dotenv import load_dotenv
 import os
 
+# Load base directory of application and load the .env file
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
 
+# Register main Flask application
 app = Flask(__name__, static_folder="static")
+# Get secret key
 app.secret_key = os.getenv("SECRET_KEY")
+# Register all the blueprints
 app.register_blueprint(authentication)
 app.register_blueprint(view)
 app.register_blueprint(public)
@@ -23,37 +27,47 @@ app.register_blueprint(create)
 app.register_blueprint(edit)
 app.register_blueprint(delete)
 
-
+# Get data configuration settings and create the database URI
 db_host = os.getenv("DATABASE_HOST")
 db_user = os.getenv("DATABASE_USER")
 db_password = os.getenv("DATABASE_PASSWORD")
 db_name = os.getenv("DATABASE_NAME")
 database_uri = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
+
+# Connect to the database
 engine = create_engine(database_uri)
 
-
+# Bind session maker to engine
 Session = sessionmaker(bind=engine, autocommit=True)
 
-
+# Flask template context processor to get all collections that belong to the user
 @app.context_processor
 def get_collections():
+    # Create database session
     db_session = Session()
     try:
+        # Query database for all collections that belong to the user
         collections = db_session.query(Collection).join(User, Collection.user_id == User.id) \
             .filter(User.email == session['user']).all()
         return dict(collections=collections)
     except:
         return dict(collections=None)
 
-
+# Flask template context processor to get all resources that belong to the user's collection
 @app.context_processor
 def resources_processor():
+    # Method for getting all resources that belong to a collection
     def get_resources(id):
+        # Create database session
         db_session = Session()
+        # Join User, Note and Collection table to check which user is authenticated
+        # Query the database for all the notes that belong to the collection using its id
         notes = db_session.query(Note).join(Collection, Note.collection_id == Collection.id) \
             .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
             .filter(Collection.user_id == User.id).filter(Note.collection_id == id).all()
 
+        # Join User, Deck and Collection table to check which user is authenticated
+        # Query the database for all the decks that belong to the collection using its id
         decks = db_session.query(Deck).join(Collection, Deck.collection_id == Collection.id) \
             .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
             .filter(Collection.user_id == User.id).filter(Deck.collection_id == id).all()
@@ -66,11 +80,16 @@ def resources_processor():
 @app.route('/', methods=['GET'])
 @requires_login
 def root():
+    # Create database session
     db_session = Session()
+    # Join User, Note and Collection table to check which user is authenticated
+    # Return first 3 notes that are the most recently updated using its timestamp
     notes = db_session.query(Note).join(Collection, Note.collection_id == Collection.id) \
         .join(User, Collection.user_id == User.id).filter(User.email == session['user'])\
         .order_by(desc('edited')).limit(3).all()
 
+    # Join User, Deck and Collection table to check which user is authenticated
+    # Return first 3 decks that are the most recently updated using its timestamp
     decks = db_session.query(Deck).join(Collection, Deck.collection_id == Collection.id) \
         .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
         .order_by(desc('edited')).limit(3).all()
@@ -80,10 +99,13 @@ def root():
 
 @app.route('/browse/', methods=['GET'])
 def browse():
+    # Create database session
     db_session = Session()
+    # Get query parameters
     query = request.args.get('query')
     if query:
         search = "%{}%".format(query)
+        # Get all collections, notes and decks that have been set to public and match the query
         searched_notes = db_session.query(Note).filter(Note.public == True).filter(Note.title.like(search)).all()
         searched_decks = db_session.query(Deck).filter(Deck.public == True).filter(Deck.title.like(search)).all()
         searched_collections = db_session.query(Collection).filter(Collection.public == True)\
@@ -99,18 +121,24 @@ def browse():
 
 @app.route('/search/', methods=['GET'])
 def search():
+    # Create database session
     db_session = Session()
+    # Get query parameters
     query = request.args.get('query')
     if query:
         search_query = "%{}%".format(query)
+        # Get all collections, notes and decks that match the query
+        # Join User, Note and Collection table to check which user is authenticated
         searched_notes = db_session.query(Note).join(Collection, Note.collection_id == Collection.id) \
             .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
             .filter(Collection.user_id == User.id).filter(Note.title.like(search_query)).all()
 
+        # Join User, Deck and Collection table to check which user is authenticated
         searched_decks = db_session.query(Deck).join(Collection, Deck.collection_id == Collection.id) \
             .join(User, Collection.user_id == User.id).filter(User.email == session['user']) \
             .filter(Collection.user_id == User.id).filter(Deck.title.like(search_query)).all()
 
+        # Join User and Collection table to check which user is authenticated
         searched_collections = db_session.query(Collection).join(User, Collection.user_id == User.id) \
             .filter(User.email == session['user']).filter(Collection.title.like(search_query)).all()
     else:
